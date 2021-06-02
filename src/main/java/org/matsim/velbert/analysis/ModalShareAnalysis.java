1f453fdf09cfb28469361259b2a5816b38153ad1
dextroseSH <pheinrich11235@gmail.com>
2021-06-02 18:26:12 +0200

    Changed EPSG code and filter for home activities

--- src/main/java/org/matsim/velbert/analysis/ModalShareAnalysis.java
+++ src/main/java/org/matsim/velbert/analysis/ModalShareAnalysis.java
@@ -15,10 +15,10 @@ import java.util.stream.Collectors;
 
 public class ModalShareAnalysis {
 
-	private static final String shapeFile = "src/main/resources/OSM_PLZ_072019.shp";
-	private static final String populationPath = "src/main/resources/velbert-v1.0-1pct.output_plans.xml.gz";
-	private static final CoordinateTransformation transformation = TransformationFactory.getCoordinateTransformation("EPSG:31468", "EPSG:3857");
-	private static final List<String> plzVelbert = Arrays.asList("42549", "42551", "42553", "42555");
+    private static final String shapeFile = "src/main/resources/OSM_PLZ_072019.shp";
+    private static final String populationPath = "src/main/resources/velbert-v1.0-1pct.output_plans.xml.gz";
+    private static final CoordinateTransformation transformation = TransformationFactory.getCoordinateTransformation("EPSG:25832", "EPSG:3857");
+    private static final List<String> plzVelbert = Arrays.asList("42549", "42551", "42553", "42555");
 
 	public static void main(String[] args) {
 		Collection<SimpleFeature> features = ShapeFileReader.getAllFeatures(shapeFile);
@@ -35,16 +35,16 @@ public class ModalShareAnalysis {
 		for (Person person : population.getPersons().values()) {
 			Plan plan = person.getSelectedPlan();
 
-			List<Activity> activities = TripStructureUtils.getActivities(plan, TripStructureUtils.StageActivityHandling.ExcludeStageActivities);
-			boolean livingInVelbert = activities.stream()
-					.filter(activity -> "h".equals(activity.getType()))
-					.map(Activity::getCoord)
-					.map(transformation::transform)
-					.map(MGC::coord2Point)
-					.allMatch(coord -> velbert.stream().anyMatch(plz -> plz.covers(coord)));
-			if (!livingInVelbert) {
-				continue;
-			}
+            List<Activity> activities = TripStructureUtils.getActivities(plan, TripStructureUtils.StageActivityHandling.ExcludeStageActivities);
+            boolean livingInVelbert = activities.stream()
+                    .filter(a -> a.getType().startsWith("home"))
+                    .map(Activity::getCoord)
+                    .map(transformation::transform)
+                    .map(MGC::coord2Point)
+                    .allMatch(coord -> velbert.stream().anyMatch(plz -> plz.covers(coord)));
+            if (!livingInVelbert) {
+                continue;
+            }
 
 
 			personCounter++;
