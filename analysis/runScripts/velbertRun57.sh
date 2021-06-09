c5dbec87b221407b8645c5099f6b98f3097200cf
dextroseSH <pheinrich11235@gmail.com>
2021-06-09 23:12:14 +0200

    Added runs 40-60

---
+++ analysis/runScripts/velbertRun57.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+#$ -cwd
+#$ -N velbert
+#$ -m be
+#$ -M p.heinrich@campus.tu-berlin.de
+#$ -j y
+#$ -o job-log-1pct57
+#$ -l h_rt=80000
+#$ -l mem_free=4G
+#$ -pe mp 4
+
+RUN_ID="velbert-v1.0-1pct"
+JAR="./matsim-velbert-0.0.1-SNAPSHOT.jar"
+MAIN_CLASS="org.matsim.velbert.RunVelbert"
+
+ARGS="-60 -5 0 ./input/config.xml --config:controler.runId $RUN_ID --config:plans.inputPlansFile matsim-velbert-v1.0-1pct.plans.xml.gz --config:controler.outputDirectory ./output-$RUN_ID-57"
+
+# make sure java is present
+module add java/11
+
+# start matsim
+java -cp $JAR -Xmx16G $MAIN_CLASS $ARGS
