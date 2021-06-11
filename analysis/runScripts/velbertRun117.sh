fd1f2105c60b65535499d58c6075c06c8080e6cd
dextroseSH <pheinrich11235@gmail.com>
2021-06-11 22:01:04 +0200

    new runs

---
+++ analysis/runScripts/velbertRun117.sh
@@ -0,0 +1,22 @@
+#!/bin/bash --login
+#$ -cwd
+#$ -N velbert
+#$ -m be
+#$ -M p.heinrich@campus.tu-berlin.de
+#$ -j y
+#$ -o job-log-1pct117
+#$ -l h_rt=80000
+#$ -l mem_free=4G
+#$ -pe mp 4
+
+RUN_ID="velbert-v1.0-1pct"
+JAR="./matsim-velbert-0.0.1-SNAPSHOT-margin.jar"
+MAIN_CLASS="org.matsim.velbert.RunVelbert"
+
+ARGS="-9 -4 0 -13 ./input/config.xml --config:controler.runId $RUN_ID --config:plans.inputPlansFile matsim-velbert-v1.0-1pct.plans.xml.gz --config:controler.outputDirectory ./output-$RUN_ID-117"
+
+# make sure java is present
+module add java/11
+
+# start matsim
+java -cp $JAR -Xmx16G $MAIN_CLASS $ARGS
